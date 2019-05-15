84ddd9585d9474e77074d0917bbe8fdb64d1db7d
t-suman <thakursuman705@gmail.com>
2019-05-15 17:13:20 +0530

    Initial commit

---
+++ app/src/main/java/com/example/uitapp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.uitapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView=findViewById(R.id.web_view);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl("http://uit.buruniv.ac.in");
+    }
+}
